f498f51089dc80ef674de7e82309a9afd8f7f5f6
Khadeeja Beevi C N <khadeejabeevinavas@gmail.com>
2024-06-10 14:46:21 +0530

    committed

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a= int(input("enter number1:"))
+b=int(input("enter number2:"))
+o=input("enter the operator:")
+
+if o=='+':
+    r=a+b
+elif o=='-':
+    r=a-b
+elif o=='*':
+    r=a*b
+elif o=='/':
+   r=a/b
+
+print(r)
+   
---
+++ loops.py
@@ -0,0 +1,11 @@
+# for loop
+list=["khadeeja","kavya","nintu","nivya","kwizera","Navas"]
+s=sorted(list)
+for i in s:
+    print(i)
+
+#while loop
+n=0
+while n<len(list):
+    print(s[n])
+    n=n+1
